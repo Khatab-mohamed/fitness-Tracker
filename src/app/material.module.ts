2ee043cfa4a3961fc84495df438a38179d24f8f9
Dev_Khatab <khatap1@hotmail.com>
2020-12-20 23:21:48 -0800

    using the material form-field , matInput in the signup

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from "@angular/core";
 
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatInputModule} from '@angular/material/input';
 @NgModule({
-    imports:[],
-    exports:[]
+    imports:[
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule
+    ],
+    exports:[
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule,
+        MatInputModule
+    ]
 })
 export class MaterialModule{}
